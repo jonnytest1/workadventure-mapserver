a3c55bcdb4cb4873cdfea884f80edb6c1ddc3e9d
jonny <ga86lad@mytum.de>
2021-12-27 23:37:40 +0100

    added validation for missing tilesets

--- resources/mapserver/service/generic-map-factory/generic-map-validator.ts
+++ resources/mapserver/service/generic-map-factory/generic-map-validator.ts
@@ -9,7 +9,12 @@ export function validateMap(mapJson: MapJson) {
 
     let layerLength = null;
 
+    const usedIndexes = new Set();
+
     mapJson.layers.forEach(layer => {
+        layer.data.forEach(index => {
+            usedIndexes.add(index);
+        });
         if (nameMap[layer.name]) {
             errors.push(`duplicate layer name: ${layer.name} this will error`);
         }
@@ -33,6 +38,16 @@ export function validateMap(mapJson: MapJson) {
         }
 
     });
+
+    usedIndexes.forEach(usedIndex => {
+        for (const tileset of mapJson.tilesets) {
+            if (tileset.firstgid < usedIndex && usedIndex < (tileset.firstgid + tileset.tilecount)) {
+                return;
+            }
+        }
+        errors.push(`the index ${usedIndex} does not have a corresponding tileset defining it`);
+    });
+
 
     errors.forEach(console.log);
     if (errors.length == 0) {
